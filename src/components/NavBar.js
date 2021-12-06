5af1d1a33df4ee99f0c21bda2c51ff4ac70039a4
Naima Hassan <naima.hassan434@gmail.com>
2021-12-06 23:39:31 +0000

    Wrap up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,36 @@
-import React,{useState} from 'react'
-import logo from '../assets/logo.png'
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import '../styles/NavBar.css';
-import DehazeIcon from '@mui/icons-material/Dehaze';
+import "../styles/NavBar.css";
+import DehazeIcon from "@mui/icons-material/Dehaze";
 
 function NavBar() {
- const [openLinks, setOpenLinks] = useState(false);
- const toggleNavbar = () => {
-  setOpenLinks(!openLinks);
- }
- return (
-  <div className="navbar">
-   <div className="leftSide" id={openLinks ? 'open' : 'close'}>
-    <img src={logo} alt="Logo" />
-    <div className="hiddenLinks">
+  const [openLinks, setOpenLinks] = useState(false);
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+  return (
+    <div className="navbar">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={logo} alt="Logo" />
+        <div className="hiddenLinks">
           <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/contact"> Contact </Link>
+          <Link to="menu"> Menu </Link>
+          <Link to="about"> About </Link>
+          <Link to="contact"> Contact </Link>
         </div>
-   </div>
-   <div className="rightSide">
-        <Link to="/">Home</Link> 
+      </div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
         <Link to="menu">Menu</Link>
         <Link to="about">About</Link>
-    <Link to="contact">Contact</Link>
-    <button onClick={toggleNavbar}>
-     <DehazeIcon/>
-    </button>
-    
-   </div>
-  </div>
- )
+        <Link to="contact">Contact</Link>
+        <button onClick={toggleNavbar}>
+          <DehazeIcon />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
