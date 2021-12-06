269cf7ce87220c43803dfe84dacf30800065a360
Naima Hassan <naima.hassan434@gmail.com>
2021-12-06 23:13:32 +0000

    Menu components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-
+import Menu from "./pages/Menu";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <NavBar/>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="menu" element={<Menu />} />
         </Routes>
-      <Footer/>
-    </div>
+        <Footer />
+      </div>
     </BrowserRouter>
-    
   );
 }
 
