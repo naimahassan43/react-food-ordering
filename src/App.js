8031472455f07fdc88388e402b217eb579c314ef
Naima Hassan <naima.hassan434@gmail.com>
2021-12-06 23:21:35 +0000

    About page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Menu from "./pages/Menu";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="menu" element={<Menu />} />
+          <Route path="about" element={<About />} />
         </Routes>
         <Footer />
       </div>
